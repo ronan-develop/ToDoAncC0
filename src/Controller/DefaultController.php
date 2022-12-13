3af9144ea6ae6e1264cba1b4b2bef8ed7361dbde
Ronan Lenouvel <ronan.lenouvel@gmail.com>
2022-12-13 18:29:41 +0100

    version(6.2.1): passed ⚠️ exception when login status 🚧

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,8 +2,8 @@
 
 namespace App\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
