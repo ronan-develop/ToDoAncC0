8a0d2eebae117d078f34dd4ae22ffc6438f0b27b
Ronan Lenouvel <ronan.lenouvel@gmail.com>
2022-12-14 11:42:07 +0100

    rector(init): set Symfony version SymfonyLevelSetList state 🚧

--- rector.php
+++ rector.php
@@ -10,7 +10,7 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->symfonyContainerXml(__DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml');
 
     $rectorConfig->sets([
-        SymfonyLevelSetList::UP_TO_SYMFONY_61,
+        SymfonyLevelSetList::UP_TO_SYMFONY_62,
         SymfonySetList::SYMFONY_CODE_QUALITY,
         SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
     ]);
