a160915379281ecc67e8eeb34025b4f08d4b7465
Ronan Lenouvel <ronan.lenouvel@gmail.com>
2023-01-02 15:03:39 +0100

    test(DefaultController): 100% status: ✅

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -2,41 +2,97 @@
 
 namespace App\Tests\Controller;
 
-use App\Repository\UserRepository;
 use Exception;
-use Generator;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Entity\User;
+use App\Tests\HelperTestCase;
+use App\Controller\DefaultController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
 
-/**
- * Class DefaultControllerTest
- * @package App\Tests
- */
-class DefaultControllerTest extends WebTestCase
+class DefaultControllerTest extends HelperTestCase
 {
     /**
-     * @return void
+     * @throws Exception
+     * @covers \App\Controller\DefaultController::index
      */
-    public function testWhenUserNotLogged(): void
+    public function testAccessHomepageByLoginForm()
     {
-        /**
-         * Shuts the kernel down if it was used in the test
-         */
-        self::ensureKernelShutdown();
-        // simulation d'une request dans symfony
-        $client = static::createClient();
-        $crawler = $client->request(Request::METHOD_GET, "/");
+        $session = new Session(new MockArraySessionStorage());
+        $session->start();
+        $session->set('user', null);
 
-        $this->assertSelectorTextContains('title', 'Redirecting to http://localhost/login');
+        $client = static::createClient();
+        $crawler =  $client->request('GET', '/');
+
+        $this->assertResponseRedirects('http://localhost/login');
+
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+
+        $crawler = $client->followRedirect();
+
+        $client->request('GET', '/login');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Connexion');
+
+        $buttonCrawlerNode = $crawler->selectButton('Se connecter');
+
+        $form = $buttonCrawlerNode->form();
+        $form['_username'] = "Admin";
+        $form['_password'] = "0000";
+
+        $client->submit($form);
         $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+        $client->followRedirect();
+        $this->assertResponseIsSuccessful(Response::HTTP_OK);
+        $this->assertSelectorTextContains(
+            "h1", "Bienvenue sur Todo List, l'application vous permettant de gérer l'ensemble de vos tâches sans effort !"
+        );
     }
 
     /**
-     * @return Generator
+     * @return void
+     * @covers \App\Controller\DefaultController::index
      */
-    public function provideUri(): Generator
+    public function testIndex(): void
     {
-        yield ['/'];
+        $session = new Session(new MockArraySessionStorage());
+        $session->start();
+        $session->set('user', null);
+
+        $client = static::createClient();
+        $client->request('GET', '/');
+        $this->assertResponseRedirects('http://localhost/login');
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        $client->followRedirect();
+
+        $client->request('GET', '/login');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Connexion');
+    }
+
+    /**
+     * @covers \App\Controller\DefaultController::index
+     * @throws Exception
+     */
+    public function testCanAccessHomepageWhenConnected()
+    {
+        $client = static::createClient();
+        $client->followRedirects();
+
+        $urlGenerator = $client->getContainer()->get('router');
+        $userRepo = $this->getEntityManager()->getRepository(User::class);
+        $user = $userRepo->findOneBy([]);
+        $client->loginUser($user);
+
+        $crawler = $client->request(
+            Request::METHOD_GET,
+            $urlGenerator->generate('homepage')
+        );
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        if(!$user) {
+            $this->assertResponseRedirects('login',Response::HTTP_FOUND);
+        }
     }
 }
